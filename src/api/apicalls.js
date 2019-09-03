f5c76908ff45d35f80c4a5ef87c35f0e54daa5b1
Hans Kristian Vik Olsen <hans.kristian.olsen@bouvet.no>
2019-09-03 13:00:04 +0200

    added stuff

--- src/api/apicalls.js
+++ src/api/apicalls.js
@@ -1,5 +1,6 @@
 //import { handleResponse, handleError } from "./apiUtils";
 
+/*
 export function getAuthors() {
   return fetch("http://localhost:6058/api/authors").then(res => res.json());
 }
@@ -20,6 +21,37 @@ export function saveAuthor(author) {
   debugger;
   console.log(JSON.stringify(author));
   return fetch("http://localhost:6058/api/authors/", {
+    method: "POST",
+    mode: "no-cors",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(author)
+  });
+}
+
+*/
+
+export function getAuthors() {
+  return fetch("http://authorappapi.azurewebsites.net/api/authors/").then(res =>
+    res.json()
+  );
+}
+
+export function getBooksFromAuthor() {
+  return fetch(
+    "http://authorappapi.azurewebsites.net/api/authors/76053df4-6687-4353-8937-b45556748abe/books/"
+  ).then(res => res.json());
+}
+
+export function getBooksFromSlug(slug) {
+  return fetch(
+    "http://authorappapi.azurewebsites.net/api/authors/" + slug + "/books/"
+  ).then(res => res.json());
+}
+
+export function saveAuthor(author) {
+  debugger;
+  console.log(JSON.stringify(author));
+  return fetch("http://authorappapi.azurewebsites.net/api/authors/", {
     method: "POST",
     mode: "no-cors",
     headers: { "content-type": "application/json" },
